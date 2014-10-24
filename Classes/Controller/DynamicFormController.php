730b829d0ef0035049442a832f47a1c6f9bf1162
Sascha Seyfert <sef@move-elevator.de>
2014-10-24 15:30:45 +0200

    #17452 add admin and user mails

--- Classes/Controller/DynamicFormController.php
+++ Classes/Controller/DynamicFormController.php
@@ -2,11 +2,13 @@
 
 namespace MoveElevator\MeDynamicForm\Controller;
 
+use MoveElevator\MeDynamicForm\Utility\MailUtility;
 use \TYPO3\CMS\Core\Utility\GeneralUtility;
 use \TYPO3\CMS\Core\Utility\HttpUtility;
 use \TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use \TYPO3\CMS\Core\Messaging\FlashMessage;
 use \TYPO3\CMS\Extbase\Persistence\Generic\QueryResult;
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use \MoveElevator\MeDynamicForm\Utility\SettingsUtility;
 use \MoveElevator\MeDynamicForm\Domain\Model\SendForm;
 
@@ -58,7 +60,6 @@ class DynamicFormController extends ActionController {
 
 	/**
 	 * @param \MoveElevator\MeDynamicForm\Domain\Model\SendForm $sendForm
-	 * @validate $sendForm MoveElevator.MeDynamicForm:SendFormValidator
 	 * @return void
 	 */
 	public function sendAction(SendForm $sendForm) {
@@ -71,8 +72,8 @@ class DynamicFormController extends ActionController {
 		if ($this->settings['mailSettings']['adminMail']['receiver']) {
 
 		}
-
 		$this->sendFormRepository->add($sendForm);
+		$this->sendEmails($sendForm);
 	}
 
 	/**
@@ -81,6 +82,63 @@ class DynamicFormController extends ActionController {
 	protected function validateSettings() {
 		if ($this->settings['formName'] === '') {
 			$this->addFlashMessage('formName is missing!', 'Error', FlashMessage::WARNING);
+		}
+	}
+
+	/**
+	 * @param \MoveElevator\MeDynamicForm\Domain\Model\SendForm $sendForm
+	 */
+	protected function sendEmails(SendForm $sendForm) {
+		//@todo make emails better readable and format values
+		if ($this->settings['mailSettings']['adminMail']['enabled'] === '1') {
+			$view = $this->objectManager->get('TYPO3\CMS\Fluid\View\StandaloneView');
+
+			$templateFile = $this->settings['mailSettings']['adminMail']['templateFile'];
+			if (!$templateFile || !is_readable($templateFile)) {
+				$view->setTemplatePathAndFilename(ExtensionManagementUtility::extPath('me_dynamic_form') . 'Resources/Private/Templates/Mails/Admin.txt');
+			} else {
+				$view->setTemplatePathAndFilename($templateFile);
+			}
+			$view->assign('settings', $this->settings);
+			$view->assign('sendForm', $sendForm);
+
+			$adminMailContent = $view->render();
+
+			MailUtility::sendMail(
+				$this->settings['mailSettings']['adminMail']['receiver'],
+				$this->settings['mailSettings']['adminMail']['from'],
+				$this->settings['mailSettings']['adminMail']['subject'],
+				$adminMailContent
+			);
+		}
+
+		if ($this->settings['mailSettings']['customerMail']['enabled'] === '1') {
+			$view = $this->objectManager->get('TYPO3\CMS\Fluid\View\StandaloneView');
+
+			$templateFile = $this->settings['mailSettings']['customerMail']['templateFile'];
+			if (!$templateFile || !is_readable($templateFile)) {
+				$view->setTemplatePathAndFilename(ExtensionManagementUtility::extPath('me_dynamic_form') . 'Resources/Private/Templates/Mails/Customer.txt');
+			} else {
+				$view->setTemplatePathAndFilename($templateFile);
+			}
+
+			$salutationFields = explode(',', $this->settings['mailSettings']['customerMail']['salutationFields']);
+			$salutation = '';
+			foreach($salutationFields as $salutationField){
+				$salutation .= $sendForm->getValueByField($salutationField) . ' ';
+			}
+
+			$view->assign('settings', $this->settings);
+			$view->assign('sendForm', $sendForm);
+			$view->assign('salutation', trim($salutation));
+			var_dump($this->settings['mailSettings']['customerMail']['receiverField']);
+			$customerMailContent = $view->render();
+			MailUtility::sendMail(
+				$sendForm->getValueByField($this->settings['mailSettings']['customerMail']['receiverField']),
+				$this->settings['mailSettings']['customerMail']['from'],
+				$this->settings['mailSettings']['customerMail']['subject'],
+				$customerMailContent
+			);
 		}
 	}
 
